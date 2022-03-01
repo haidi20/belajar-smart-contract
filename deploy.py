fcf12f84ea1824d1047e13ba45e646cf82773a9e
haidi <haidinurhadinata@gmail.com>
2022-03-01 10:45:22 +0800

    updating contract dengan masukkan nilai ke store di solidity

--- deploy.py
+++ deploy.py
@@ -55,6 +55,7 @@ nonce = w3.eth.getTransactionCount(my_address)
 # 1. Build a transaction
 # 2. Sign a transaction
 # 3. Send a transaction
+# PART 1
 transaction = SimpleStorage.constructor().buildTransaction(
     {
         "gasPrice": w3.eth.gas_price,
@@ -67,8 +68,14 @@ transaction = SimpleStorage.constructor().buildTransaction(
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 
 # Send this signed transaction
+print("Deploying contract...")
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+# END PART 1
+
+
+# PART 2
 
 # Working with contract, you need:
 # Contract ABI
@@ -76,5 +83,25 @@ tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 # Call -> Simulate making the call and getting a return value
 # Transact -> Actually make a state change
-print(simple_storage.functions.store(15).call())
 print(simple_storage.functions.getVal().call())
+print("Updating Contract...")
+
+# initial value of favorite number
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction,
+    private_key=private_key,
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.getVal().call())
+# END PART 2
